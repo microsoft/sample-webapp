9a56e767515a69034d90dc5b6a674a6f05184f1f
Kashish Gupta <kashishgupta2508@gmail.com>
2026-07-07 18:27:30 +0530

    Add FAQ page with accordion feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/faq">FAQ</Link>
         <Link to="/login">Login</Link>
         <Link to="/dashboard">Dashboard</Link>
         <ThemeToggle />
