8ef1a0b2d5404262e9367b35f9736670a677dfe2
Kashish Gupta <kashishgupta2508@gmail.com>
2026-08-06 20:51:59 +0530

    feat: update Home page welcome headline

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
+﻿import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to Sample Web App</h1>
+      <h1>Welcome to SampleApp -- Built for Developers</h1>
       <p id="description">A React web application built for Playwright testing.</p>
       <div className="actions">
         <Link to="/login" className="btn btn-primary">Get Started</Link>
